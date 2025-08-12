2374bb986b5d2e68220c1b38c7f475d86a31197e
hbhangale3 <hbhangale3@gmail.com>
2025-08-11 21:43:06 -0700

    Added functionality to display local and remote images

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,7 +1,25 @@
+import mapsImg from '@/images/maps.jpg'
+import Image from 'next/image';
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
+
+
 function page({params}:{params: {id:String}}){
     return(
         <div>
             <h1 className="text-4xl">ID: {params.id}</h1>
+            <section className='flex gap-x-4 mt-4'>
+                {/* local image */}
+                <div>
+                <Image src={mapsImg} width={192} height={192} alt='maps' priority className='w-48 h-48 object-cover rounded'/>
+                <h2>local image</h2>
+                </div>
+                {/* remote image */}
+                <div>
+                <Image src={url} alt='tours' width={192} height={192} priority className='w-48 h-48 object-cover rounded' />
+                <h2>remote image</h2>
+                </div>
+            </section>
+
         </div>
     )
 }
